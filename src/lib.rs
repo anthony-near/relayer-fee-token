e0605de1171ef0b24beed213a8369dfa85f6a4e3
anthony-near <anthony@near.org>
2023-10-26 14:47:53 -0500

    updated near_sdk_contract_tools to 2.0.0, much simplified code

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,19 @@
 // generated using https://near.org/contractwizard.near/widget/ContractWizardUI
 
 use near_sdk::{
-    AccountId, PanicOnDefault, borsh::{BorshDeserialize, BorshSerialize, self},
-    env, json_types::{Base64VecU8, U128}, near_bindgen, store::Vector,
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    env,
+    json_types::{Base64VecU8, U128},
+    near_bindgen,
+    store::Vector,
+    PanicOnDefault,
 };
-use near_sdk_contract_tools::{
-    FungibleToken, hook::Hook, Owner, standard::nep141::*, ft::*
-};
+use near_sdk_contract_tools::ft::*;
 
-#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault, Owner, FungibleToken)]
-#[fungible_token(transfer_hook = "TransferHook")]
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault, FungibleToken)]
 #[near_bindgen]
 pub struct Contract {
     blobs: Vector<Vec<u8>>,
-}
-
-pub struct TransferHook;
-
-impl Hook<Contract, Nep141Transfer<'_>> for TransferHook {
-    fn hook<R>(
-        contract: &mut Contract,
-        args: &Nep141Transfer<'_>,
-        f: impl FnOnce(&mut Contract) -> R,
-    ) -> R {
-        contract.require_registration(args.receiver_id);
-        f(contract)
-    }
 }
 
 #[near_bindgen]
@@ -42,9 +30,18 @@ impl Contract {
             24,
         ));
 
-        Nep141Controller::mint(&mut contract, "relayer-fee-token.testnet".parse().unwrap(), 1000000000, None);
-
         contract
+    }
+
+    pub fn mint(&mut self, amount: U128) {
+        Nep141Controller::mint(
+            self,
+            &Nep141Mint {
+                amount: amount.into(),
+                receiver_id: &env::predecessor_account_id(),
+                memo: None,
+            },
+        ).unwrap();
     }
 
     pub fn use_storage(&mut self, blob: Base64VecU8) {
@@ -56,20 +53,6 @@ impl Contract {
         self.lock_storage(
             &env::predecessor_account_id(),
             ((storage_end - storage_start) as u128 * env::storage_byte_cost()).into(),
-        )
-            .unwrap_or_else(|e| env::panic_str(&format!("Storage lock error: {}", e)));
-    }
-
-    fn require_registration(&self, account_id: &AccountId) {
-        self.get_storage_balance(account_id)
-            .unwrap_or_else(|e| env::panic_str(&e.to_string()));
-    }
-
-    pub fn mint(&mut self, account_id: AccountId, amount: U128) {
-        Nep141Controller::mint(self, account_id, amount.into(), None);
-    }
-
-    pub fn burn(&mut self, amount: U128) {
-        Nep141Controller::burn(self, env::predecessor_account_id(), amount.into(), None);
+        ).unwrap_or_else(|e| env::panic_str(&format!("Storage lock error: {}", e)));
     }
 }
